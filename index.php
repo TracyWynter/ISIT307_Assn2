d9e5c3ac508b6cf76d6ffdd934e5038551ba01f1
Tracy <57853991+TracyWynter@users.noreply.github.com>
2021-01-28 22:33:57 +0800

    Added CSS for the main page

--- index.php
+++ index.php
@@ -2,80 +2,21 @@
     <head>
         <title>Take a challenge</title>
         <!-- Styling -->
-        <style type="text/css">
-            body{
-                text-align:center;
-                font-family: Arial, Helvetica, sans-serif;
-                padding: 20px;
-            }
-
-            /* Quiz Category Radio Selection */
-            .hideRadio{
-                /*hide the radio button */
-                display:none;
-            }
-            /* Category Button Label */
-            .btnLabel{
-                width:50px;
-                display: inline-block;
-                cursor: pointer;
-                border-radius: 4px;
-                background: dimgrey;
-                color:white;
-                padding: 2px 5px;
-            }
-            .btnLabel:hover{
-                background:grey;
-            }
-            /* When radio btn is checked */
-            #movies:checked + .btnLabel ,
-            #music:checked + .btnLabel,
-            #sport:checked + .btnLabel {
-                background:teal;
-                color:white;
-            }
-
-            /* Game Form */
-            .gameStartForm{
-                margin-left:auto;
-                margin-right:auto;
-                width: 700px;
-            }
-            /* Nickname Input */
-            #name{
-                border:1.5px solid grey;
-                border-radius:2px;
-                padding: 3px 5px;
-            }
-
-            /* Start Game (Submit) Button */
-            .startGameBtn{
-                margin-left: auto;
-                margin-right:auto;
-            }
-            .startGameBtn input[type=submit]{
-                padding:2px 6px;
-            }
-            .startGameBtn input[type=submit]:hover{
-                cursor: pointer;
-            }
-
-
-        </style>
+        <link rel="stylesheet" href="gameMenu.css">
     </head>
     <body>
         <!-- PHP Scripts -->
         <?php
         session_start();
         $gameArr = array(
-            'name' => '',
+            'user' => '',
             'category' => ''
         );
-         $errMsg = "";
+        $errMsg = "";
         $checked = TRUE;  // Only if true then will proceed to start game
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (!empty($_POST["name"])) {
-                $gameArr['name'] =  $_POST["name"];
+            if (!empty($_POST["user"])) {
+                $gameArr['user'] = $_POST["user"];
             } else {
                 $checked = False;
             }
@@ -86,7 +27,7 @@
             }
 
             if ($checked) {
-                $_SESSION["name"] = $gameArr["name"];
+                $_SESSION["user"] = $gameArr["user"];
                 header("Location:game.php?category=" . $gameArr['category']);
             } else {
                 $errMsg = "Please fill in your nickname and select <b>one</b> category to start game";
@@ -94,24 +35,18 @@
         }
         ?>
         <div>
+            <p id="logo"></p>
+        </div>
+        <div>
+            <p id="challengeTitle">Take a Challenge</p>
             <form class="gameStartForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"/>
-            <p><label for="name" >Nickname: </label><input type="text" name="name" id="name" value="<?php echo $gameArr['name'];?>"></p>
-            <p><label for="category">Choose 1:</label>
-                <input type="radio" class="hideRadio" name="category" id="movies" value="Movies"<?php if ($gameArr['category'] == "Movies") {
-            echo "checked";
-        } ?>/><label for="movies" class="btnLabel">Movies</label>
-                <input type="radio" class="hideRadio" name="category" id="music" value="Music" <?php if ($gameArr['category'] == "Music") {
-            echo "checked";
-        } ?> /><label for="music" class="btnLabel">Music</label>
-                <input type="radio" class="hideRadio" name="category" id="sport" value="Sport"/><label for="sport" class="btnLabel">Sport</label>  
-            </p>
-            <div class="startGameBtn">
-                <input type="submit" name="submit" value="Start Game">
-            </div>  
-            <div>
-                <p><?php echo $errMsg; ?></p>
-            </div>
-        </form>
-    </div>
+            <p><input type="text" name="user" id="user" placeholder="Enter your Nickname" value="<?php echo $gameArr['user']; ?>"></p>
+                <?php require 'gameMenu.php'; ?>
+        </div>
+        <div>
+            <p><?php echo $errMsg; ?></p>
+        </div>
+    </form>
+
 </body>
 </html>
