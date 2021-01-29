1cc7911b48c63f9acf2d29b94fc4ba0cf9f48c35
Tracy <57853991+TracyWynter@users.noreply.github.com>
2021-01-30 00:31:35 +0800

    Modify CSS

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
     <head>
         <title>Take a challenge</title>
         <!-- Styling -->
-        <link rel="stylesheet" href="gameMenu.css">
+        <link rel="stylesheet" href="gameMenu.css"/>
     </head>
     <body>
         <!-- PHP Scripts -->
         <?php
+        session_unset();
         session_start();
         $gameArr = array(
             'user' => '',
@@ -15,35 +16,40 @@
         $errMsg = "";
         $checked = TRUE;  // Only if true then will proceed to start game
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (!empty($_POST["user"])) {
-                $gameArr['user'] = $_POST["user"];
-            } else {
-                $checked = False;
-            }
-            if (!empty($_POST["category"])) {
-                $gameArr['category'] = $_POST["category"];
-            } else {
-                $checked = False;
-            }
+            // Do not do check when first get redirected from the exit page 
+            if (!isset($_POST['new_game'])) { // Prevent printing err msg
+                if (!empty($_POST["user"])) {
+                    $gameArr['user'] = $_POST["user"];
+                } else {
+                    $checked = False;
+                }
+                if (!empty($_POST["category"])) {
+                    $gameArr['category'] = $_POST["category"];
+                } else {
+                    $checked = False;
+                }
 
-            if ($checked) {
-                $_SESSION["user"] = $gameArr["user"];
-                header("Location:game.php?category=" . $gameArr['category']);
-            } else {
-                $errMsg = "Please fill in your nickname and select <b>one</b> category to start game";
+                if ($checked) {
+                    $_SESSION["user"] = $gameArr["user"];
+                    header("Location:game.php?category=" . $gameArr['category']);
+                } else {
+                    $errMsg = "Please fill in your nickname and select <b>one</b> category to start game";
+                }
             }
         }
         ?>
+        <!-- Game Logo -->
         <div>
             <p id="logo"></p>
         </div>
+        <!-- Game Start Menu Prompt -->
         <div>
             <p id="challengeTitle">Take a Challenge</p>
             <form class="gameStartForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"/>
             <p><input type="text" name="user" id="user" placeholder="Enter your Nickname" value="<?php echo $gameArr['user']; ?>"></p>
                 <?php require 'gameMenu.php'; ?>
         </div>
-        <div>
+        <div class="errMsg">
             <p><?php echo $errMsg; ?></p>
         </div>
     </form>
