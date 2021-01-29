310df318c7030cfcbd4c81b727d651b28032cea0
Tracy <57853991+TracyWynter@users.noreply.github.com>
2021-01-30 00:32:23 +0800

    Modify CSS and add validation

--- currentResult.php
+++ currentResult.php
@@ -3,9 +3,14 @@
         <title>Game Result</title>
         <link rel="stylesheet" href="gameMenu.css"> <!-- Game Menu CSS import -->
         <style type="text/css">
+            body{
+                font-family: "Comic Sans MS", "Comic Sans", cursive;  
+                padding-bottom:35px;
+                background-image: linear-gradient(to bottom right, mediumblue,teal ,indigo ,darkcyan, midnightblue);
+            }
             .resultBoard{
                 width: 400px;
-                height: 160px;
+                height:410px;
                 margin-left:auto;
                 margin-right:auto;
                 border-radius:15px;
@@ -23,23 +28,32 @@
                 background-color:darkslategray;
                 color:white;
                 border-radius:15px;
-                font-family: Arial, Helvetica, sans-serif;
                 font-size:35px;
             }
-            #btns{
+            #nextRound{
                 margin-left:auto;
                 margin-right:auto;
-                width: 400px;
-                padding:10px;
+                width: 220px;
+                padding:5px;
+                border-radius:50px;
+                background-color:powderblue;
+                color:black;
+            }
+            .err{
+                margin-bottom:8px;
+                color:red;
+                height:20px;
             }
             #exitBtn{
-                padding:4px 6px;
+                padding:6px 8px;
                 cursor:pointer;
                 background:brown;
                 color:white;
                 border:none;
-                border-radius: 2px;
+                border-radius: 4px;
+                outline:none;
             }
+
         </style>
     </head>
     <body>
@@ -49,19 +63,22 @@
             'user' => '',
             'category' => ''
         );
-        $errMsg ="";
+        $errMsg = "";
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if (isset($_POST['exit_game'])) {
                 header("Location:finalResult.php"); // Direct to the ending result of the challenge
             }
-            if(isset($_POST['start_game'])&&!empty($_POST['category'])){
-                header("Location:game.php?category=".$_POST['category']);   // A new round of challenge
-            }
-            else{
+            if (isset($_POST['start_game']) && !empty($_POST['category'])) {
+                header("Location:game.php?category=" . $_POST['category']);   // A new round of challenge
+            } else {
                 $errMsg = "Select one category";
             }
         }
         ?>
+        <!-- Game Logo -->
+        <div>
+            <p id="logo"></p>
+        </div>
         <!-- Display Result -->
         <div class="resultBoard">
             <div id="resultTitle">Results</div>
@@ -72,13 +89,13 @@
             <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
                 <input type="submit" id="exitBtn" name="exit_game" value="Exit Game">
             </form>
+            <div class="err"><?php echo $errMsg; ?></div>
+            <div id="nextRound">
+                <p>Proceed to next round ... </p>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+                    <?php require 'gameMenu.php'; ?>
+                </form>
+            </div>
         </div>
-        <div id="btns">
-            <p>Proceed to next round ... </p>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-                <?php require 'gameMenu.php'; ?>
-            </form>
-        </div>
-        <div><?php echo $errMsg;?></div>
     </body>
 </html>
